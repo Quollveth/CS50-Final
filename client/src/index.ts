117542ef033848c2701a86f08e6bfff5f17a01f5
quollveth <klaus.marrer@outlook.com>
2024-05-15 14:00:39 -0300

    form generator

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,39 +1,33 @@
 import $ from 'jquery';
 
+import { createForm } from './scripts/formCreator';
+
 const SERVER_IP = 'http://127.0.0.1:8080';
+let userForm: HTMLFormElement;
 
-document.addEventListener('DOMContentLoaded', function () {
-  const form = document.getElementById('registerForm') as HTMLFormElement;
-
-  form.addEventListener('submit', function (event) {
-    event.preventDefault();
-    const username = (<HTMLInputElement>document.getElementById('username'))
-      .value;
-    const email = (<HTMLInputElement>document.getElementById('email')).value;
-    const password = (<HTMLInputElement>document.getElementById('password'))
-      .value;
-    const confirmPassword = (<HTMLInputElement>(
-      document.getElementById('confirmPassword')
-    )).value;
-
-    // Basic validation
-    if (!username || !email || !password || !confirmPassword) {
-      alert('All fields are required');
-      return;
-    }
-
-    if (password !== confirmPassword) {
-      alert('Passwords do not match');
-      return;
-    }
-
-    $.post(`${SERVER_IP}/test`, { username, email, password })
-      .done(function (response) {
-        alert('Registration successful');
-        console.log(response);
-      })
-      .fail(function (error) {
-        alert('Registration failed');
-      });
-  });
+// On document load
+$(function () {
+  userForm = document.getElementById('user-form-body') as HTMLFormElement;
+  /*
+  userForm = document.createElement('form') as HTMLFormElement;
+  userForm.classList.add('user-form');
+  userForm.id = 'user-form-body';
+  document.appendChild(userForm);
+  */
+  createForm('LOGIN', userForm);
 });
+
+/*
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  $.post(`${SERVER_IP}/login`, {})
+    .done(function (response) {
+      alert('Login successful');
+      console.log(response);
+    })
+    .fail(function (error) {
+      alert('Login failed');
+    });
+});
+*/
