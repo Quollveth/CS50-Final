7f9f97f68f944d5be47b42b03038927cf2fc76d9
quollveth <klaus.marrer@outlook.com>
2024-05-15 16:21:33 -0300

    form swapping

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,18 +1,38 @@
 import $ from 'jquery';
 
 import { createForm } from './scripts/formCreator';
+import type { formType } from './scripts/formCreator';
 
 const SERVER_IP = 'http://127.0.0.1:8080';
 let userForm: HTMLFormElement;
 
+let currentForm: formType = 'LOGIN';
+
+const swapForms = () => {
+  userForm.innerHTML = ''; // Remove all children
+  currentForm = currentForm == 'LOGIN' ? 'REGISTER' : 'LOGIN'; // Swap form type
+  createForm(currentForm, userForm); // Create new form
+  const swapButton = document.getElementById(
+    'change-button'
+  ) as HTMLButtonElement;
+  swapButton.addEventListener('click', swapForms);
+};
+
 // On document load
 $(function () {
+  // Create login page
   userForm = document.createElement('form') as HTMLFormElement;
   userForm.classList.add('user-form');
   userForm.id = 'user-form-body';
   document.body.appendChild(userForm);
 
-  createForm('LOGIN', userForm);
+  createForm(currentForm, userForm);
+
+  // Swap between login and register page
+  const swapButton = document.getElementById(
+    'change-button'
+  ) as HTMLButtonElement;
+  swapButton.addEventListener('click', swapForms);
 });
 
 /*
--- client/src/scripts/formCreator.ts
+++ client/src/scripts/formCreator.ts
@@ -58,6 +58,7 @@ const createInput = (
 };
 
 export function createForm(type: formType, root: HTMLFormElement) {
+  // Create input fields
   root.appendChild(createInput('USER'));
   root.appendChild(createInput('PASS'));
   if (type == 'REGISTER') {
@@ -65,6 +66,7 @@ export function createForm(type: formType, root: HTMLFormElement) {
     root.appendChild(createInput('CONFIRM'));
   }
 
+  // Create buttons
   let submit = document.createElement('button') as HTMLButtonElement;
   submit.classList.add('user-form');
   submit.id = 'submit-button';
@@ -76,8 +78,29 @@ export function createForm(type: formType, root: HTMLFormElement) {
   change.classList.add('user-form');
   change.id = 'change-button';
 
+  let changeLabel = document.createElement('label') as HTMLLabelElement;
+  changeLabel.setAttribute('for', change.id);
+  changeLabel.classList.add('user-form');
+  changeLabel.id = 'change-label';
+
+  changeLabel.innerText =
+    type == 'REGISTER'
+      ? 'Already have an account?'
+      : "Don't yet have an account?";
   change.innerText = type == 'REGISTER' ? 'Log In' : 'Register';
 
-  root.appendChild(submit);
-  root.appendChild(change);
+  // Create divs to organize the buttons
+  let container = document.createElement('div') as HTMLDivElement;
+  let container2 = document.createElement('div') as HTMLDivElement;
+
+  container.classList.add('user-form');
+  container.id = 'buttons';
+
+  container.appendChild(submit);
+
+  container2.appendChild(changeLabel);
+  container2.appendChild(change);
+
+  container.appendChild(container2);
+  root.appendChild(container);
 }
