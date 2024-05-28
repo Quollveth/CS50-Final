509d2113419b8fb3f32a307097703fbf9ae27410
quollveth <klaus.marrer@outlook.com>
2024-05-28 20:50:50 -0300

    login required decorator

--- server/src/helpers/validation.py
+++ server/src/helpers/validation.py
@@ -1,3 +1,21 @@
+from functools import wraps
+from flask import session
+
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    https://flask.palletsprojects.com/en/latest/patterns/viewdecorators/
+    From CS50 Finance
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user") is None:
+            return '',401 # Return 401 unauthorized
+        return f(*args, **kwargs)
+
+    return decorated_function
+   
 def validateUsername(username):
     return True
 
